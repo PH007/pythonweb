c67af39eeb79c5c195f501dd6642873ffc792511
Pham Hoang Thai Loc <locpht@allexceed.vn>
2018-08-15 10:44:17 +0700

    change settings

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's1-cps4bm13kj0#ppxdckhy(7cixv72ogqkgk&ovhk9frd%x8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
