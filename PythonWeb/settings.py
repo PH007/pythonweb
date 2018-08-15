c83ed9f5ae5654872927dce15b94a95da55e30b0
Pham Hoang Thai Loc <locpht@allexceed.vn>
2018-08-15 10:37:22 +0700

    update

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's1-cps4bm13kj0#ppxdckhy(7cixv72ogqkgk&ovhk9frd%x8-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
