e118b849a4e8913bade6e1bec26305132b668e4b
PH007 <phamhoangthailoc@gmail.com>
2018-08-15 09:50:59 +0700

    Update settings.py

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's1-cps4bm13kj0#ppxdckhy(7cixv72ogqkgk&ovhk9frd%x8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
